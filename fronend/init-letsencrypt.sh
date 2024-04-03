b646b4ca2b2443cc3ab9ff10f73be36f8d2889df
MariAro833 <masha.rozhnova@gmail.com>
2024-04-03 10:00:31 +0300

    clean

--- fronend/init-letsencrypt.sh
+++
@@ -1,4 +0,0 @@
-domains=(aurinkolab.fi www.aurinkolab.fi)
-email="info@aurinkolab.fi"
-
-curl -L https://raw.githubusercontent.com/wmnnd/nginx-certbot/master/init-letsencrypt.sh > init-letsencrypt.sh
--- fronend/src/utils/animation.ts
+++
@@ -1,9 +0,0 @@
-export const animationStart = 0.43;
-
-export const reveal =  {
-    hiddenVariant: { y: 50, opacity: 0 },
-    revealedVariant: { 
-        y: 0,
-        opacity: 1, 
-    },
-};
