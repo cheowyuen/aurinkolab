f05a32281889aee34d9cd664139509308e5799c2
Yuen <cheowyuen@gmail.com>
2024-04-12 11:50:06 +0300

    Added events and event details page

---
+++ frontend/src/AllEvents.tsx
@@ -0,0 +1,42 @@
+import { useNavigate } from 'react-router-dom';
+import event_img from '/src/assets/aalto-design-factory.jpeg';
+
+const AllEvents = () => {   
+    const navigate = useNavigate();
+
+    const events = [
+        {image: event_img, place: "Espoo", date: "Apr - May 2024", eventId: 1},
+        {image: event_img, place: "Espoo", date: "Aug - Dec 2024", eventId: 2},
+        {image: event_img, place: "Espoo", date: "Jan - Jun 2025", eventId: 3},
+        {image: event_img, place: "Espoo", date: "TBA", eventId: 4},
+    ]
+
+    const handleEventClick = (eventId: number) => {
+        navigate(`/events/${eventId}`);
+    };
+
+    return (
+        <div>
+            <div className='events-title'>
+                <p>Events</p>
+            </div>
+
+            <div className="flex justify-center p-16 events-page">
+                <div className="grid lg:grid-cols-3 md:grid-cols-2 sm:grid-cols-1 gap-12">
+                    {events.map((event) => (
+                        <div className="relative w-80 h-60 bg-gray-300 rounded-xl overflow-hidden cursor-pointer event-container" onClick={() => handleEventClick(event.eventId)}> 
+                            <img src={event.image} alt={event.place} className="absolute w-full h-full object-cover opacity-80" /> 
+                            <div className="absolute inset-0 bg-black opacity-30 overlay"></div>
+                            <div className="absolute inset-0 flex items-center justify-center flex-col">
+                                <p className="text-center text-white">{event.place}</p>
+                                <p className="text-center text-white">{event.date}</p>
+                            </div>
+                        </div>
+                    ))}
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default AllEvents;
---
+++ frontend/src/EventDetails.tsx
@@ -0,0 +1,68 @@
+//import { useParams } from 'react-router-dom';
+import event_img from '/src/assets/aalto-design-factory.jpeg';
+import date_icon from '/src/assets/date-icon.png';
+import edu_centre_icon from '/src/assets/education-centre.png';
+import place_icon from '/src/assets/location.png';
+import vehicle_icon from '/src/assets/boat-icon.png';
+import engine_icon from '/src/assets/engine.png';
+import schedule_icon from '/src/assets/schedule.png';
+
+function EventDetails() {
+    //const { eventId } = useParams(); 
+  
+    const event = {
+        eventId: 1, 
+        eventName: "Engineering Hackathon - 1", 
+        dates: "April - May 2024", 
+        educationCentre: "Aurinko Lab",
+        place: "Keran Hallit, Aalto Design Factory", 
+        vehicle: "Boat",
+        engine: "Solar Electric",
+        regatta: "01 June 2024"
+    };
+
+    return (
+        <div>
+            <div className='event-title'>
+                <p>{event.eventName}</p>
+            </div>
+
+            <div className="flex flex-wrap md:flex-nowrap">
+                <div className="flex-1 p-15">
+                    <img src={event_img} alt="Vehicle" className="max-w-full h-auto rounded-lg shadow-md" />
+                </div>
+
+                <div className="flex-1 p-15 text-xl">
+                    <p className="text-3xl title-font">Event Details</p><br/>
+                    <span className="icon-text">
+                        <img src={date_icon} alt="Date" className="icon" />
+                        <span className="icon-details"><span className="font-bold">Dates</span>: {event.dates}</span>
+                    </span><br/><br/>
+                    <span className="icon-text">
+                        <img src={edu_centre_icon} alt="Education Centre" className="icon" />
+                        <span className="icon-details"><span className="font-bold">Education Centre</span>: {event.educationCentre}</span>
+                    </span><br/><br/>
+                    <span className="icon-text">
+                        <img src={place_icon} alt="Place" className="icon" />
+                        <span className="icon-details"><span className="font-bold">Place</span>: {event.place}</span>
+                    </span><br/><br/>
+                    <span className="icon-text">
+                        <img src={vehicle_icon} alt="Vehicle" className="icon" />
+                        <span className="icon-details"><span className="font-bold">Vehicle</span>: {event.vehicle}</span>
+                    </span><br/><br/>
+                    <span className="icon-text">
+                        <img src={engine_icon} alt="Engine" className="icon" />
+                        <span className="icon-details"><span className="font-bold">Engine</span>: {event.engine}</span>
+                    </span><br/><br/>
+                    <span className="icon-text">
+                        <img src={schedule_icon} alt="Schedule" className="icon" />
+                        <span className="icon-details"><span className="font-bold">Schedule</span>: </span>
+                    </span><br/><br/>
+                    <button className="button-small">Apply</button>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default EventDetails;
