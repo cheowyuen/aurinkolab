cf8e31d1a9cfc64ef577df76006464884b5d8d54
MariAro833 <masha.rozhnova@gmail.com>
2024-04-24 11:43:50 +0300

    updated text for About AurinkoLab

--- frontend/src/AboutAurinkoLab.tsx
+++ frontend/src/AboutAurinkoLab.tsx
@@ -15,16 +15,17 @@ function AboutAurinkoLabSection() {
           />
           <div className="description-text">
             <p>
-              📚 AurinkoLab Resource Hub<br/><br/>
-              Access a diverse range of materials such as diagrams, manuals, and training videos. Presently featuring materials for solar-electric boats and go-karts.
-              Hydrogen-powered vehicles and bikes will soon be added to our lineup.<br/><br/>
-              ⏱️ 20-Hour Hackathon for Engineers<br/><br/>
-              20-hour education program designed for 15-21 year-olds to create their own electric-solar and hydrogen-driven vehicles.
-              The program has been accepted as TET practice by the educational administration of Espoo city. Also Helsinki Business College and Interlang Education Club are interested in sending their teams to participate.<br/><br/>
+              ⏱️ 20-Hour Engineering Hackathon<br/><br/>
+              20-hour education program designed for 15-** year-olds to create their own electric-solar and hydrogen-driven vehicles.<br/>
+              The program has been accepted as TET practice by the educational administration of Espoo city.<br/><br/>
               🏆 Final Championship<br/><br/>
-              The hackathon concludes with a thrilling final championship event, where teams proudly showcase their innovative vehicles<br/><br/>
+              The hackathon concludes with a final championship event - Solar Regatta or Racing - where teams showcase their innovative vehicles<br/><br/>
               🌞 AurinkoLab Engineering Talent Hub<br/><br/>
-              Engineering Talent Hub is a community for engineering students and enthusiasts to design their own hydrogen-driven or electric-solar vehicles, using established or original designs. AurinkoLab experts rigorously test these solutions, integrating them into the AurinkoLab Resource Hub for future hackathons. Talented students are recommended to partner companies in need of skilled employees.<br/><br/>
+              Engineering Talent Hub is a community for engineering students and enthusiasts to design their own hydrogen-driven or electric-solar vehicles, using established or original designs. <br/>
+              AurinkoLab experts rigorously test these solutions, integrating them into the AurinkoLab Resource Hub for future hackathons. <br/>
+              Talented students are recommended to partner companies in need of skilled employees.<br/><br/>
+              📚 AurinkoLab Resource Hub<br/><br/>
+              Access a diverse range of materials such as diagrams, manuals, and training videos.<br/><br/>
             </p>
           </div>
         </div>
