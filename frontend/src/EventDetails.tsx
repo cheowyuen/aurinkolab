5f99227694b9819d1491911c5d8a37484955dd19
Yuen <cheowyuen@gmail.com>
2024-04-25 11:48:11 +0300

    Updated events and event details

--- frontend/src/EventDetails.tsx
+++ frontend/src/EventDetails.tsx
@@ -1,46 +1,70 @@
-//import { useParams } from 'react-router-dom';
-import event_img from '/src/assets/aalto-design-factory.jpeg';
+import { useState, useEffect } from 'react';
+import { useParams } from 'react-router-dom';
+import allEvents from '../src/data/allEvents';
 import date_icon from '/src/assets/date-icon.png';
 import edu_centre_icon from '/src/assets/education-centre.png';
 import place_icon from '/src/assets/location.png';
 import vehicle_icon from '/src/assets/boat-icon.png';
 import engine_icon from '/src/assets/engine.png';
+import tutor_icon from '/src/assets/tutor.png';
 import schedule_icon from '/src/assets/schedule.png';
 
+export interface Event {
+    eventId: number;
+    eventName: string;
+    dates: string;
+    educationCenter: string;
+    place: string;
+    vehicle: string;
+    engine: string;
+    tutor: string;
+    schedule: string;
+    status: string;
+    image: string;
+}
+
 function EventDetails() {
-    //const { eventId } = useParams(); 
-  
-    const event = {
-        eventId: 1, 
-        eventName: "Engineering Hackathon - 1", 
-        dates: "April - May 2024", 
-        educationCentre: "Aurinko Lab",
-        place: "Keran Hallit, Aalto Design Factory", 
-        vehicle: "Boat",
-        engine: "Solar Electric",
-        regatta: "01 June 2024"
-    };
+    const { eventId } = useParams(); 
+    const [ events, setEvents ] = useState<Event[]>([]);
+    let title = "Ongoing Event";
+
+    useEffect(() => {
+        setEvents(allEvents); 
+    }, [])
+
+    const event = events.find(e => e.eventId === Number(eventId))
+
+    if (!event) {
+        return <div>No event found</div>;
+    }
+
+    if (event.status === "upcoming") {
+        title = "Upcoming Event";
+    }
+    else if (event.status === "archive") {
+        title = "Past Event";
+    }
 
     return (
         <div>
             <div className='event-title' data-testid="event-details-title">
-                <p>{event.eventName}</p>
+                <p>{title}</p>
             </div>
 
             <div className="flex flex-wrap md:flex-nowrap">
                 <div className="flex-1 event-image-container">
-                    <img src={event_img} alt="Vehicle" className="max-w-full h-auto rounded-lg shadow-md vehicle-image" />
+                    <img src={event.image} alt="Vehicle" className="max-w-full h-auto rounded-lg shadow-md vehicle-image" />
                 </div>
 
                 <div className="flex-1 text-xl event-text-container">
-                    <p className="text-3xl title-font">Event Details</p><br/>
+                    <p className="text-3xl title-font">{event.eventName}</p><br/>
                     <span className="icon-text">
                         <img src={date_icon} alt="Date" className="icon" />
                         <span className="icon-details"><span className="font-bold">Dates</span>: {event.dates}</span>
                     </span><br/><br/>
                     <span className="icon-text">
                         <img src={edu_centre_icon} alt="Education Centre" className="icon" />
-                        <span className="icon-details"><span className="font-bold">Education Center</span>: {event.educationCentre}</span>
+                        <span className="icon-details"><span className="font-bold">Education Center</span>: {event.educationCenter}</span>
                     </span><br/><br/>
                     <span className="icon-text">
                         <img src={place_icon} alt="Place" className="icon" />
@@ -54,11 +78,19 @@ function EventDetails() {
                         <img src={engine_icon} alt="Engine" className="icon" />
                         <span className="icon-details"><span className="font-bold">Engine</span>: {event.engine}</span>
                     </span><br/><br/>
+                    {event.tutor != "" && (
+                        <>
+                            <span className="icon-text">
+                                <img src={tutor_icon} alt="Tutor" className="icon" />
+                                <span className="icon-details"><span className="font-bold">Tutor</span>: {event.tutor}</span>
+                            </span><br/><br/>
+                        </>
+                    )}   
                     <span className="icon-text">
                         <img src={schedule_icon} alt="Schedule" className="icon" />
                         <span className="icon-details"><span className="font-bold">Schedule</span>: </span>
                     </span><br/><br/>
-                    <button className="button-small">Apply</button>
+                    {/* <button className="button-small">Apply</button> */}
                 </div>
             </div>
         </div>
@@ -66,3 +98,4 @@ function EventDetails() {
 }
 
 export default EventDetails;
+
---
+++ frontend/src/data/allEvents.tsx
@@ -0,0 +1,123 @@
+import { Event } from "../EventDetails";
+
+const allEvents: Event[] = [
+    {
+        eventId: 1, 
+        eventName: "Visit to Keravan Energia's Solar Plant", 
+        dates: "04 Apr 2024", 
+        educationCenter: "Aurinko Lab",
+        place: "Kerava, Finland", 
+        vehicle: "N/A",
+        engine: "N/A",
+        tutor: "",
+        schedule: "",
+        status: "archive", 
+        image: "/src/assets/Kerava.jpg"
+    },
+    {
+        eventId: 2, 
+        eventName: "Engineering Hackathon-1", 
+        dates: "Apr - May 2024", 
+        educationCenter: "Aurinko Lab",
+        place: "Keran Hallit, Aalto Design Factory", 
+        vehicle: "Blue Boat",
+        engine: "Solar-electric",
+        tutor: "Tony",
+        schedule: "",
+        status: "ongoing", 
+        image: "/src/assets/Aalto.jpeg"
+    },
+    {
+        eventId: 3, 
+        eventName: "Engineering Hackathon-2", 
+        dates: "Apr - May 2024", 
+        educationCenter: "Aurinko Lab",
+        place: "Keran Hallit, Aalto Design Factory", 
+        vehicle: "Orange Boat",
+        engine: "Solar-electric",
+        tutor: "Tony",
+        schedule: "",
+        status: "ongoing", 
+        image: "/src/assets/Aalto.jpeg"
+    },
+    {
+        eventId: 4, 
+        eventName: "Engineering Hackathon-3", 
+        dates: "Apr - May 2024", 
+        educationCenter: "Aurinko Lab",
+        place: "Sigulda, Latvia", 
+        vehicle: "Green Boat",
+        engine: "Solar-electric",
+        tutor: "Rostislav",
+        schedule: "",
+        status: "ongoing", 
+        image: "/src/assets/Sigulda.png"
+    },
+    {
+        eventId: 5, 
+        eventName: "Solar Regatta", 
+        dates: "01 Jun 2024", 
+        educationCenter: "Aurinko Lab",
+        place: "Helsinki, Finland", 
+        vehicle: "Boat",
+        engine: "Solar-electric",
+        tutor: "",
+        schedule: "",
+        status: "upcoming", 
+        image: "/src/assets/Helsinki.png"
+    },
+    {
+        eventId: 6, 
+        eventName: "Training for Tutors", 
+        dates: "28 - 29 Aug 2024", 
+        educationCenter: "Aurinko Lab",
+        place: "Online", 
+        vehicle: "Boat",
+        engine: "Hydrogen-driven and solar-electric",
+        tutor: "",
+        schedule: "",
+        status: "upcoming", 
+        image: "/src/assets/Aalto.jpeg"
+    },
+    {
+        eventId: 7, 
+        eventName: "Engineering Hackathon-4", 
+        dates: "Sep - Oct 2024", 
+        educationCenter: "Aurinko Lab",
+        place: "Keran Hallit, Aalto Design Factory", 
+        vehicle: "Go-karts",
+        engine: "Solar-electric",
+        tutor: "Tony",
+        schedule: "",
+        status: "upcoming", 
+        image: "/src/assets/Aalto.jpeg"
+    },
+    {
+        eventId: 8, 
+        eventName: "Engineering Hackathon-5", 
+        dates: "Sep - Oct 2024", 
+        educationCenter: "Aurinko Lab",
+        place: "Keran Hallit, Aalto Design Factory", 
+        vehicle: "Go-karts",
+        engine: "Solar-electric",
+        tutor: "Tony",
+        schedule: "",
+        status: "upcoming", 
+        image: "/src/assets/Aalto.jpeg"
+    },
+    {
+        eventId: 9, 
+        eventName: "Solar Race", 
+        dates: "26 Oct 2024", 
+        educationCenter: "Aurinko Lab",
+        place: "Vantaa, Finland", 
+        vehicle: "Go-karts",
+        engine: "Solar-engine",
+        tutor: "",
+        schedule: "",
+        status: "upcoming", 
+        image: "/src/assets/Vantaa.jpg"
+    },
+];
+
+export default allEvents;
