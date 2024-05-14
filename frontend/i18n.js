2541b2fd49fc9a872fc795dbc52b4ae4c15e75d2
Andrés Gómez <gomezandres331@gmail.com>
2024-05-14 15:23:55 +0300

    addding i18n

---
+++ frontend/i18n.js
@@ -0,0 +1,19 @@
+import i18next from "i18next"
+
+
+
+i18next.init({
+  //  debug:true,
+    fallbackLng: 'en',
+  
+    resources:{
+        en:{
+            translation:{
+                question1:"una prueba importante"
+            }
+
+            }
+        }
+    }
+);
+
