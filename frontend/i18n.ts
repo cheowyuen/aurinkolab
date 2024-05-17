ae860e1bf9718570dd50ab64ea1798343ab44cd2
Andrés Gómez <gomezandres331@gmail.com>
2024-05-17 18:08:18 +0300

    adding backend package for i18next

--- frontend/i18n.ts
+++ frontend/i18n.ts
@@ -2,9 +2,9 @@ import i18next from "i18next"
 import {initReactI18next} from 'react-i18next'
 import ChainedBackend from "i18next-chained-backend";
 import LanguageDetector from 'i18next-browser-languagedetector'
+import Backend from 'i18next-http-backend'
 
-import enTranslation from "../frontend/public/locales/en/translation.json"
-import esTranslation from "../frontend/public/locales/es/translation.json"
+
 
 
 
@@ -13,20 +13,13 @@ i18next
 .use(initReactI18next)
 .use(ChainedBackend)
 .use(LanguageDetector)
+.use(Backend)
+
 .init({
     preload: ['en', 'es'],
     debug:true,
     fallbackLng:'en',
-    defaultNS: 'translation',
-    resources:{
-        en:{
-            translation: enTranslation
-        },
-        es:{
-            translation: esTranslation
-        }
-    },
-   
+
       });
 
 export default i18next
